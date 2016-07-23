11e9666fd0da630d771985a03f6b96b9393aefb7
João Simões <nogravity00@outlook.com>
2016-07-23 02:00:30 +0100

    Implemented base interface for pure systems

---
+++ src/SimpleISO7064/Iso7064.cs
@@ -0,0 +1,50 @@
+﻿namespace SimpleISO7064
+{
+    /// <summary>
+    /// An ISO 7064 Pure System provider to validate or 
+    /// compute check digits
+    /// </summary>
+    public interface IIso7064PureSystemProvider
+    {
+        /// <summary>
+        /// The pure system modulus
+        /// </summary>
+        int Modulus { get; }
+
+        /// <summary>
+        /// The pure system radiz
+        /// </summary>
+        int Radix { get; }
+
+        /// <summary>
+        /// The supported character set
+        /// </summary>
+        char[] CharacterSet { get; }
+
+        /// <summary>
+        /// Is the computed check digit composed by two characters?
+        /// </summary>
+        bool IsDoubleCheckDigit { get; }
+
+        /// <summary>
+        /// Checks if the given value contains a valid check digit.
+        /// </summary>
+        /// <param name="computedValue">The value to check</param>
+        /// <returns></returns>
+        bool IsValid(string computedValue);
+
+        /// <summary>
+        /// Computes the check digit from a given value.
+        /// </summary>
+        /// <param name="value">The value from which the check digit will be computed</param>
+        /// <returns>The check digit</returns>
+        string ComputeCheckDigit(string value);
+
+        /// <summary>
+        /// Computes the check digit and appends it to the given value.
+        /// </summary>
+        /// <param name="value">The value from which the check digit will be computed</param>
+        /// <returns>The value and appended check digit</returns>
+        string Compute(string value);
+    }
+}
