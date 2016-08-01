57b80506c8d8b8f36fe4620cf9db418d620561be
João Simões <nogravity00@outlook.com>
2016-08-01 01:44:20 +0100

    Removed static from methods

--- src/SimpleISO7064/Iso7064Factory.cs
+++ src/SimpleISO7064/Iso7064Factory.cs
@@ -32,7 +32,7 @@ namespace SimpleISO7064
         /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
         /// </summary>
         /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
-        public static Iso7064PureSystemProvider GetMod11Radix2()
+        public Iso7064PureSystemProvider GetMod11Radix2()
         {
             return new Iso7064PureSystemProvider(11, 2, false, "0123456789X");
         }
@@ -41,7 +41,7 @@ namespace SimpleISO7064
         /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
         /// </summary>
         /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
-        public static Iso7064PureSystemProvider GetMod37Radix2()
+        public Iso7064PureSystemProvider GetMod37Radix2()
         {
             return new Iso7064PureSystemProvider(37, 2, false, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*");
         }
@@ -50,7 +50,7 @@ namespace SimpleISO7064
         /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
         /// </summary>
         /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
-        public static Iso7064PureSystemProvider GetMod97Radix10()
+        public Iso7064PureSystemProvider GetMod97Radix10()
         {
             return new Iso7064PureSystemProvider(97, 10, true, "0123456789");
         }
@@ -59,7 +59,7 @@ namespace SimpleISO7064
         /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
         /// </summary>
         /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
-        public static Iso7064PureSystemProvider GetMod661Radix26()
+        public Iso7064PureSystemProvider GetMod661Radix26()
         {
             return new Iso7064PureSystemProvider(661, 26, true, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         }
@@ -68,7 +68,7 @@ namespace SimpleISO7064
         /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
         /// </summary>
         /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
-        public static Iso7064PureSystemProvider GetMod1271Radix36()
+        public Iso7064PureSystemProvider GetMod1271Radix36()
         {
             return new Iso7064PureSystemProvider(1271, 36, true, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXZY");
         }
