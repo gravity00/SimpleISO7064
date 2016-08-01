fbe532e6da1f786e8dc41603972017be685fcd96
João Simões <nogravity00@outlook.com>
2016-08-01 01:51:53 +0100

    Updated documentation

--- src/SimpleISO7064/Iso7064Factory.cs
+++ src/SimpleISO7064/Iso7064Factory.cs
@@ -38,36 +38,36 @@ namespace SimpleISO7064
         }
 
         /// <summary>
-        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
+        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 37 and Radix 2
         /// </summary>
-        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
+        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 37 and Radix 2</returns>
         public Iso7064PureSystemProvider GetMod37Radix2()
         {
             return new Iso7064PureSystemProvider(37, 2, false, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*");
         }
 
         /// <summary>
-        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
+        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 97 and Radix 10
         /// </summary>
-        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
+        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 97 and Radix 10</returns>
         public Iso7064PureSystemProvider GetMod97Radix10()
         {
             return new Iso7064PureSystemProvider(97, 10, true, "0123456789");
         }
 
         /// <summary>
-        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
+        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 661 and Radix 26
         /// </summary>
-        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
+        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 661 and Radix 26</returns>
         public Iso7064PureSystemProvider GetMod661Radix26()
         {
             return new Iso7064PureSystemProvider(661, 26, true, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         }
 
         /// <summary>
-        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 11 and Radix 2
+        /// Creates a insatnce of <see cref="Iso7064PureSystemProvider"/> with Modulus 1271 and Radix 36
         /// </summary>
-        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 11 and Radix 2</returns>
+        /// <returns>An ISO 7064 Pure System provider to validate or compute check digits, with Modulus 1271 and Radix 36</returns>
         public Iso7064PureSystemProvider GetMod1271Radix36()
         {
             return new Iso7064PureSystemProvider(1271, 36, true, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXZY");
